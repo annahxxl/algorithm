31992fd4ea8259350d581ac1cbe37fc78061cf1a
Hanna Lee <8annahxxl@gmail.com>
2021-07-07 19:15:54 +0900

    Add : Binary Search Tree 테스트 코드 추가

--- data-structures/binary-search-tree.py
+++ data-structures/binary-search-tree.py
@@ -117,14 +117,33 @@ class NodeMgmt:
 
         return True
     
-head = Node(1)
-BST = NodeMgmt(head)
-BST.insert(2)
-BST.insert(3)
-BST.insert(0)
-BST.insert(4)
-BST.insert(8)
-BST.delete(8)
-BST.delete(0)
+# 0 ~ 999 숫자 중에서 임의로 100개를 추출해서, 이진 탐색 트리에 입력, 검색, 삭제
+import random
 
-print(BST.search(0))
+# 0 ~ 999 중, 100 개의 숫자 랜덤 선택
+bst_nums = set()
+while len(bst_nums) < 100:
+    bst_nums.add(random.randint(0, 999))
+# print (bst_nums)
+
+# 선택된 100개의 숫자를 이진 탐색 트리에 입력, 임의로 루트노드는 500을 넣기로 함
+head = Node(500)
+binary_tree = NodeMgmt(head)
+for num in bst_nums:
+    binary_tree.insert(num)
+    
+# 입력한 100개의 숫자 검색 (검색 기능 확인)
+for num in bst_nums:
+    if binary_tree.search(num) == False:
+        print ('search failed', num)
+
+# 입력한 100개의 숫자 중 10개의 숫자를 랜덤 선택
+delete_nums = set()
+bst_nums = list(bst_nums)
+while len(delete_nums) < 10:
+    delete_nums.add(bst_nums[random.randint(0, 99)])
+
+# 선택한 10개의 숫자를 삭제 (삭제 기능 확인)
+for del_num in delete_nums:
+    if binary_tree.delete(del_num) == False:
+        print('delete failed', del_num)
