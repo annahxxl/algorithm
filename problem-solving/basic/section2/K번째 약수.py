ef48e9da1cbbb4bd8d092c8a592187d37d40286a
Hanna Lee <8annahxxl@gmail.com>
2021-08-03 15:35:44 +0900

    [Basic] section2 - K번째 약수

--- problem-solving/basic/section2/K번째 약수.py
+++ problem-solving/basic/section2/K번째 약수.py
@@ -1,10 +1,25 @@
+'''
+# my solution
+n, k = map(int, input().split())
+nums = []
+for i in range(1, n+1):
+  if n % i == 0:
+    nums.append(i)
+nums.sort()
+if k > len(nums):
+  print(-1)
+else:
+  print(nums[k-1])
+'''
+
+# other solution
 n, k = map(int, input().split())
 cnt = 0
-
 for i in range(1, n+1):
   if n % i == 0:
     cnt += 1
   if cnt == k:
     print(i)
     break
-else: print(-1)
+else: # for문이 정상적으로 종료 시 실행
+  print(-1)
