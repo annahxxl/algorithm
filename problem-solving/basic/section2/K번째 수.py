708a2ed63dc49cbca65f98b20c7b75465989d6a8
Hanna Lee <8annahxxl@gmail.com>
2021-08-05 18:00:32 +0900

    [Basic] section2 - K번째 수

--- problem-solving/basic/section2/K번째 수.py
+++ problem-solving/basic/section2/K번째 수.py
@@ -1,8 +1,6 @@
 t = int(input())
-
 for i in range(t):
   n, s, e, k = map(int, input().split())
   nums = list(map(int, input().split()))
-  nums = nums[s-1 : e]
-  nums.sort()
-  print('#%d %d' %(i+1, nums[k-1]))
+  nums = sorted(nums[s-1 : e])
+  print("#%d %d" %(i+1, nums[k-1]))
