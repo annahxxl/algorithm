ac9e191b68e5f77fbac77f2c0f8014e0085ec0f7
Hanna Lee <8annahxxl@gmail.com>
2021-08-08 12:03:54 +0900

    [Basic] K번째 큰 수

--- problem-solving/basic/section2/K번째 큰 수.py
+++ problem-solving/basic/section2/K번째 큰 수.py
@@ -1,12 +1,9 @@
 n, k = map(int, input().split())
 cards = list(map(int, input().split()))
-sums = set()
-
-for i in range(n):
+result = set()
+for i in range(1, n):
   for j in range(i+1, n):
     for t in range(j+1, n):
-      sums.add(cards[i] + cards[j] + cards[t]) # set자료구조는 add
-      
-sums = list(sums) # set자료구조는 중복❌, 순서❌ -> sort개념❌ -> 리스트로 바꿔야함
-sums.sort(reverse = True)
-print(sums[k-1])
+      result.add(cards[i] + cards[j] + cards[t])
+result = sorted(result, reverse=True)
+print(result[k-1])
