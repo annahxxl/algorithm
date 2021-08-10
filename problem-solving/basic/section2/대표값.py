b98915ef2cf72b5b75b8d89986ae14931b5b77c0
Hanna Lee <8annahxxl@gmail.com>
2021-08-10 11:48:01 +0900

    [Basic] 대표값

--- problem-solving/basic/section2/대표값.py
+++ problem-solving/basic/section2/대표값.py
@@ -1,20 +1,16 @@
 n = int(input())
 scores = list(map(int, input().split()))
-# avg = round(sum(scores) / n) # 파이썬에서 round는 round_half_even방식을 택함
-avg = sum(scores) / n
-avg = avg + 0.5
-avg = int(avg)
-min = 2147000000
-
-for idx, score in enumerate(scores): # 리스트 index와 value를 쌍으로 리턴
-  diff = abs(score - avg)
-  if diff < min:
-    min = diff
-    res = score # 점수가 평균에 가까운 학생의 점수 저장
-    student = idx + 1 # 점수가 평균에 가까운 학생의 번호 저장
-  elif diff == min:
-    if score > res:
-      res = score
-      student = idx + 1
-      
-print(avg, student)
+# avg = round(sum(scores)/n) # 파이썬에서 round는 round_half_even 방식이므로 아래 방법으로 수정
+avg = int(sum(scores)/n + 0.5)
+min_diff = 2147000000
+for i in range(n):
+  diff = abs(avg-scores[i])
+  if diff < min_diff:
+    min_diff = diff
+    result = i+1
+    score = scores[i]
+  elif diff == min_diff:
+    if score < scores[i]:
+      score = scores[i]
+      result = i+1
+print(avg, result)
