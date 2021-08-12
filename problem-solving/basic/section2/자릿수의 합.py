94e4922e81e60aa2af9f7837a91177fba64410e1
Hanna Lee <8annahxxl@gmail.com>
2021-08-12 10:59:16 +0900

    [Basic] 정다면체, 자릿수의 합

--- problem-solving/basic/section2/자릿수의 합.py
+++ problem-solving/basic/section2/자릿수의 합.py
@@ -1,6 +1,27 @@
+'''
+# my solution
+n = int(input())
+nums = list(input().split())
+sums = list() # 자릿수의 합
+
+def digit_sum(x):
+  sum = 0
+  for i in x:
+    sum += int(i)
+  return sum
+
+for num in nums:
+  result = digit_sum(num)
+  sums.append(result)
+MAX = max(sums)
+for i in range(n):
+  if sums[i] == MAX:
+    print(nums[i])
+'''
+
+# other solution
 n = int(input())
 nums = list(map(int, input().split()))
-max = 0
 
 def digit_sum(x):
   sum = 0
@@ -9,10 +30,10 @@ def digit_sum(x):
     x = x // 10
   return sum
 
-for num in nums:
-  sum = digit_sum(num)
-  if sum > max:
-    max = sum
-    res = num
-    
+MAX = -2147000000
+for x in nums:
+  sum = digit_sum(x)
+  if sum > MAX:
+    MAX = sum
+    res = x
 print(res)
--- problem-solving/basic/section2/정다면체.py
+++ problem-solving/basic/section2/정다면체.py
@@ -1,16 +1,10 @@
 n, m = map(int, input().split())
-sum_cnt = [0] * (n+m) # 눈의 합을 카운트 해주는 리스트 (리스트의 index가 점수)
-max = 0
-# max = -2147000000
-
-for i in range(1, n+1): # 눈의 합 횟수 저장
+sums_cnt = [0] * (n+m+1) # index가 나올 수 있는 눈의 합
+for i in range(1, n+1):
   for j in range(1, m+1):
-    sum_cnt[i+j] += 1
-    
-for i in range(n+m+1): # 최대값 저장
-  if sum_cnt[i] > max:
-    max = sum_cnt[i]
-    
-for i in range(n+m+1): # 출력
-  if sum_cnt[i] == max:
+    sum = i + j
+    sums_cnt[sum] += 1
+max_cnt = max(sums_cnt)
+for i in range(len(sums_cnt)):
+  if sums_cnt[i] == max_cnt:
     print(i, end=' ')
