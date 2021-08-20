7a097b0aaa373666f040f1e651365893bcd841ae
Hanna Lee <8annahxxl@gmail.com>
2021-08-21 06:35:32 +0900

    [Basic] 수들의 합

--- problem-solving/basic/section3/수들의 합.py
+++ problem-solving/basic/section3/수들의 합.py
@@ -1,24 +1,31 @@
+'''
+# my solution (시간 초과)
 n, m = map(int, input().split())
-nums = list(map(int, input().split()))
-
-lp = 0 # left pointer
-rp = 1 # right pointer
-tot = nums[0] # nums[lp] 부터 nums[rp] 직전 값의 합
+lst = list(map(int, input().split()))
 cnt = 0
+for i in range(n):
+  for j in range(n):
+    if sum(lst[i:j+1]) == m:
+      cnt += 1
+print(cnt)
+'''
 
+# other solution: index 포인터를 이용한 모든 경우의 수 구하기
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+tot = lst[0]
+lp = rp = cnt = 0
 while True:
   if tot < m:
-    if rp < n:
-      tot += nums[rp]
-      rp += 1
-    else: # rp = n
+    rp += 1
+    if rp == n:
       break
+    tot += lst[rp]
   elif tot == m:
     cnt += 1
-    tot -= nums[lp]
+    tot -= lst[lp]
     lp += 1
   else:
-    tot -= nums[lp]
+    tot -= lst[lp]
     lp += 1
-    
 print(cnt)
