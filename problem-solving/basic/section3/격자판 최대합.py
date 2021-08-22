0a1a84b05b347ee6260b65a5b6dcc81981722909
Hanna Lee <8annahxxl@gmail.com>
2021-08-22 23:57:24 +0900

    [Basic] 격자판 최대합

--- problem-solving/basic/section3/격자판 최대합.py
+++ problem-solving/basic/section3/격자판 최대합.py
@@ -1,30 +1,43 @@
+'''
+# my solution
+n = int(input())
+grid = list()
+for _ in range(n):
+  grid.append(list(map(int, input().split())))
+SUM = list()
+cross_sum_1 = cross_sum_2 = 0
+for i in range(n):
+  SUM.append(sum(grid[i])) # 행의 합
+  cross_sum_1 += grid[i][i] # 대각선의 합
+  cross_sum_2 += grid[i][n-i-1]
+  col_sum = 0
+  for j in range(n): # 열의 합
+    col_sum += grid[j][i]
+    SUM.append(col_sum)
+SUM.append(cross_sum_1)
+SUM.append(cross_sum_2)
+print(max(SUM))
+'''
+
+# other solution
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-largest = -2147000000
-
-# # 격자 모양 출력
-# for i in grid:
-#   print(i)
-
-# 행, 열의 합
+MAX = -2147000000
 for i in range(n):
-  row = col = 0
+  row = col = 0 # 행의 합, 열의 합
   for j in range(n):
     row += grid[i][j]
     col += grid[j][i]
-  if row > largest:
-    largest = row
-  if col > largest:
-    largest = col
-    
-# 대각선의 합
-cross1 = cross2 = 0
+  if row > MAX:
+    MAX = row
+  if col > MAX:
+    MAX = col
+sum1 = sum2 = 0 # 대각선의 합
 for i in range(n):
-  cross1 += grid[i][i]
-  cross2 += grid[i][n-i-1]
-if cross1 > largest:
-  largest = cross1
-if cross2 > largest:
-  largest = cross2
-    
-print(largest)
+  sum1 += grid[i][i]
+  sum2 += grid[i][n-i-1]
+if sum1 > MAX:
+    MAX = sum1
+if sum2 > MAX:
+  MAX = sum2
+print(MAX)
