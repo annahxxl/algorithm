096f4ee0b7d88144e204fcf09ef37058c9b7b3b9
Hanna Lee <8annahxxl@gmail.com>
2021-08-25 16:11:02 +0900

    [Basic] 봉우리

--- problem-solving/basic/section3/봉우리.py
+++ problem-solving/basic/section3/봉우리.py
@@ -1,26 +1,39 @@
+'''
+# my solution
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
-
-# 격자의 가장자리 0으로 초기화
 grid.insert(0, [0]*n)
 grid.append([0]*n)
-for row in grid:
-  row.insert(0, 0)
-  row.append(0)
-
-# 봉우리 세기
+for i in range(n+2):
+  grid[i].insert(0, 0)
+  grid[i].append(0)
+cnt = 0
 for i in range(1, n+1):
   for j in range(1, n+1):
-      if grid[i][j] > max(grid[i-1][j], grid[i][j+1], grid[i+1][j], grid[i][j-1]):
-        cnt += 1
-        
-# # sol2
-# dx = [-1, 0, 1, 0] # 행 (상, 하)
-# dy = [0, 1, 0, -1] # 열 (좌, 우)
-# for i in range(1, n+1):
-#     for j in range(1, n+1):
-#         if all(grid[i][j]>grid[i+dx[k]][j+dy[k]] for k in range(4)):
-#             cnt += 1
-            
+    if grid[i][j] > grid[i][j-1] and grid[i][j] > grid[i-1][j] and grid[i][j] > grid[i][j+1] and grid[i][j] > grid[i+1][j]:
+      cnt += 1
+print(cnt)
+'''
+
+# other solution
+import sys
+input = sys.stdin.readline
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+grid.insert(0, [0]*n)
+grid.append([0]*n)
+for i in grid:
+  i.insert(0, 0)
+  i.append(0)
+
+cnt = 0
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    if all(grid[i][j] > grid[i+dx[k]][j+dy[k]] for k in range(4)): # all(조건식): 조건식이 모두 만족될 때 True
+      cnt += 1
+
 print(cnt)
