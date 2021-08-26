19f0954d8970203283439fe9a5825acd86887cbc
Hanna Lee <8annahxxl@gmail.com>
2021-08-26 21:57:54 +0900

    [Basic] 스도쿠 검사

--- problem-solving/basic/section3/스도쿠 검사.py
+++ problem-solving/basic/section3/스도쿠 검사.py
@@ -1,19 +1,14 @@
-sudoku = [list(map(int, input().split())) for _ in range(9)]
-
 def check(sudoku):
-  # 행, 열 체크
   for i in range(9):
-    row = [0] * 10 # 행 체크 리스트 -> 가로 줄
-    col = [0] * 10 # 열 체크 리스트 -> 세로 줄
+    row = col = [0] * 10
     for j in range(9):
       row[sudoku[i][j]] = 1
       col[sudoku[j][i]] = 1
-    if (sum(row) != 9) or (sum(col) != 9):
+    if sum(row) != 9 or sum(col) != 9:
       return False
-  # 그룹 체크
   for i in range(3):
     for j in range(3):
-      group = [0] * 10 # 그룹 체크 리스트
+      group = [0] * 10
       for k in range(3):
         for t in range(3):
           group[sudoku[i*3+k][j*3+t]] = 1
@@ -21,6 +16,7 @@ def check(sudoku):
         return False
   return True
 
+sudoku = [list(map(int, input().split())) for _ in range(9)]
 if check(sudoku):
   print('YES')
 else:
