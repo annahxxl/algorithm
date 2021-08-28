18cf3a9a7c5860ec01d72ad2f6f30f3f1a8f449e
Hanna Lee <8annahxxl@gmail.com>
2021-08-28 15:04:55 +0900

    [Basic] 이분검색

--- problem-solving/basic/section4/이분검색.py
+++ problem-solving/basic/section4/이분검색.py
@@ -1,15 +1,13 @@
 n, m = map(int, input().split())
-nums = list(map(int, input().split()))
-nums.sort()
-lp = 0 # 왼쪽 포인터 (index)
-rp = n-1 # 오른쪽 포인터 (index)
-
+nums = sorted(list(map(int, input().split())))
+lp = 0
+rp = n - 1
 while lp <= rp:
-  mid = (lp+rp) // 2
-  if nums[mid] == m:
-    print(mid+1)
+  mid = (lp + rp) // 2
+  if m == nums[mid]:
+    print(mid + 1)
     break
-  elif nums[mid] > m:
+  elif m < nums[mid]:
     rp = mid - 1
-  else:
+  elif m > nums[mid]:
     lp = mid + 1
