fd3f69325df28ed85696f72b855f67a6589daa83
Hanna Lee <8annahxxl@gmail.com>
2021-08-29 11:34:07 +0900

    [Basic] 랜선자르기(결정알고리즘)

--- problem-solving/basic/section4/랜선자르기_결정알고리즘.py
+++ problem-solving/basic/section4/랜선자르기_결정알고리즘.py
@@ -1,29 +1,22 @@
-k, n = map(int, input().split())
-line = []
-res = 0
-largest = 0 # 가장 긴 랜선
+# 이분검색을 이용한 결정알고리즘을 사용하여 풀 수 있는 문제 특징:
+# 특정 범위 안에 답이 있다는 것을 알 수 있을 경우
 
-for _ in range(k):
-  tmp = int(input())
-  line.append(tmp)
-  largest = max(largest, tmp)
-  
-def count(len): # 만들어진 랜선 개수 구하기
+def Count(length):
   cnt = 0
-  for i in line:
-    cnt += (i//len)
+  for line in lines:
+    cnt += (line // length)
   return cnt
 
-# 이분 검색
+k, n = map(int, input().split())
+lines = sorted([int(input()) for _ in range(k)])
+MAX = max(lines)
 lp = 1
-rp = largest
-
+rp = MAX
 while lp <= rp:
-  mid = (lp+rp) // 2
-  if count(mid) >= n:
-    res = mid
+  mid = (lp + rp) // 2
+  if Count(mid) >= n:
+    result = mid
     lp = mid + 1
   else:
     rp = mid - 1
-    
-print(res)
+print(result)
