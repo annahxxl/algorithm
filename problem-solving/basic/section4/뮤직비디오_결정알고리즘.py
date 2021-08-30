ce86da0401e6a0c6a3cd3e9fd572274ff20e04cf
Hanna Lee <8annahxxl@gmail.com>
2021-08-30 23:23:01 +0900

    [Basic] 뮤직비디오(결정알고리즘)

--- problem-solving/basic/section4/뮤직비디오_결정알고리즘.py
+++ problem-solving/basic/section4/뮤직비디오_결정알고리즘.py
@@ -1,27 +1,23 @@
-n, m = map(int, input().split())
-music = list(map(int, input().split()))
-
-def count(capacity): # DVD 개수 구하기
+def Count(capacity): # DVD 개수 구하기
   cnt = 1
-  sum = 0
+  SUM = 0
   for i in music:
-    if sum+i > capacity:
+    if SUM + i > capacity:
       cnt += 1
-      sum = i
+      SUM = i
     else:
-      sum += i
+      SUM += i
   return cnt
-
+  
+n, m = map(int, input().split())
+music = list(map(int, input().split())) # 노래 길이
 lp = max(music)
 rp = sum(music)
-res = 0
-
-while lp <= rp:
-  mid = (lp+rp) // 2
-  if count(mid) <= m:
+while lp <= rp: # 최소 용량 크기 구하기
+  mid = (lp + rp) // 2
+  if Count(mid) <= m:
     res = mid
     rp = mid - 1
   else:
     lp = mid + 1
-    
 print(res)
