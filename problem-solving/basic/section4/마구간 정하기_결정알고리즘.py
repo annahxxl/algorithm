305366c9bebf82018944f05646574c0874c59858
Hanna Lee <8annahxxl@gmail.com>
2021-08-31 21:18:37 +0900

    [Basic] 마구간 정하기(결정알고리즘)

--- problem-solving/basic/section4/마구간 정하기_결정알고리즘.py
+++ problem-solving/basic/section4/마구간 정하기_결정알고리즘.py
@@ -1,29 +1,22 @@
-n, c = map(int, input().split())
-loc = []
-
-for _ in range(n):
-  loc.append(int(input()))
-  
-loc.sort()
-
-def count(len): # 마구간에 배치할 수 있는 말 수 구하기
+def Count(distance): # 말을 넣을 수 있는 개수
   cnt = 1
-  ep = loc[0] # 말이 마지막으로 배치된 마구간 위치 (endpoint)
+  ep = loc[0] # 현재 마지막으로 말을 넣은 지점
   for i in range(1, n):
-    if loc[i]-ep >= len:
+    if loc[i] - ep >= distance:
       cnt += 1
       ep = loc[i]
   return cnt
 
-lp = 1
+n, c = map(int, input().split())
+loc = [int(input()) for _ in range(n)]
+lp = 0
 rp = loc[n-1]
-
+loc.sort()
 while lp <= rp:
-  mid = (lp+rp) // 2
-  if count(mid) >= c:
-    res = mid
+  mid = (lp + rp) // 2 # 간격
+  if Count(mid) >= c:
+    result = mid
     lp = mid + 1
   else:
     rp = mid - 1
-    
-print(res)
+print(result)
