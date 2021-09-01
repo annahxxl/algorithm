1a7f0ff7774eb390098c8effdcd3d734015e2520
Hanna Lee <8annahxxl@gmail.com>
2021-09-01 21:47:05 +0900

    [Basic] 회의실 배정(그리디)

--- problem-solving/basic/section4/회의실 배정_그리디.py
+++ problem-solving/basic/section4/회의실 배정_그리디.py
@@ -1,19 +1,13 @@
 n = int(input())
-meeting = []
-
+meeting = list()
 for _ in range(n):
   s, e = map(int, input().split())
   meeting.append((s, e))
-  
-# 정렬 기준을 e로 바꾸고 정렬하기
-meeting.sort(key = lambda x : (x[1], x[0]))
-
-et = 0 # end time
+meeting.sort(key=lambda x: (x[1], x[0]))
 cnt = 0
-
+et = 0 # end time
 for s, e in meeting:
   if s >= et:
     cnt += 1
     et = e
-    
 print(cnt)
