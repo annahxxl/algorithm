e0c65fea08d644249f856c47c08d4eda164ebc8f
Hanna Lee <8annahxxl@gmail.com>
2021-09-02 14:57:42 +0900

    [Basic] 씨름 선수(그리디)

--- problem-solving/basic/section4/씨름 선수_그리디.py
+++ problem-solving/basic/section4/씨름 선수_그리디.py
@@ -1,18 +1,35 @@
+'''
+# my solution
 n = int(input())
-body = []
-
+player = list()
 for _ in range(n):
   height, weight = map(int, input().split())
-  body.append((height, weight))
-  
-body.sort(reverse=True)
-
-largest = 0 # 몸무게 최대값
+  player.append((height, weight))
+player.sort() # 1) 키 순서로 정렬
 cnt = 0
-
-for height, weight in body:
-  if weight > largest:
+MAX = 0
+for i in range(n): # 2) 몸무게 비교
+  MAX = player[i][1]
+  for j in range(i+1, n):
+    weight = player[j][1]
+    if weight > MAX:
+      break
+  else:
     cnt += 1
-    largest = weight
-    
+print(cnt)
+'''
+
+# other solution
+n = int(input())
+player = list()
+for _ in range(n):
+  height, weight = map(int, input().split())
+  player.append((height, weight))
+player.sort(reverse=True)
+MAX = 0
+cnt = 0
+for height, weight in player:
+  if weight > MAX:
+    MAX = weight
+    cnt += 1
 print(cnt)
