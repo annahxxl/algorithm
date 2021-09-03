3704dc66ed069ae6b324b9dba8e3640c70addad8
Hanna Lee <8annahxxl@gmail.com>
2021-09-04 01:05:11 +0900

    [Basic] 침몰하는 타이타닉(그리디)

--- problem-solving/basic/section4/침몰하는 타이타닉_그리디.py
+++ problem-solving/basic/section4/침몰하는 타이타닉_그리디.py
@@ -1,39 +1,14 @@
-n, m = map(int, input().split())
-weight = list(map(int, input().split()))
-
-weight.sort()
-
-cnt = 0
-
-# while weight:
-#   if len(weight) == 1:
-#     cnt += 1
-#     break
-#   if weight[0]+weight[-1] > m:
-#     weight.pop()
-#     cnt += 1
-#   else:
-#     weight.pop(0)
-#     weight.pop()
-#     cnt += 1
-    
-# sol2 - 덱(deque) 자료구조 사용
-# list에서 pop 사용 시 뒤의 자료들이 앞으로 당겨지는 연산을 함
-# 반면에 덱(deque) 자료구조는 자료들이 움직이지 않음
 from collections import deque
 
-weight = deque(weight)
-    
+n, m = map(int, input().split())
+weight = deque(sorted(list(map(int, input().split()))))
+cnt = 0
 while weight:
-  if len(weight) == 1:
-    cnt += 1
-    break
-  if weight[0]+weight[-1] > m:
-    weight.pop()
-    cnt += 1
-  else:
+  if len(weight) >= 2 and weight[0] + weight[-1] <= m:
     weight.popleft()
     weight.pop()
     cnt += 1
-
+  else:
+    weight.pop()
+    cnt += 1
 print(cnt)
