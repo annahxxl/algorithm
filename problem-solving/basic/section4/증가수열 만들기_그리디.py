510d4a20a0a7e7e8f205a01815a97ace1eedf97e
Hanna Lee <8annahxxl@gmail.com>
2021-09-05 21:20:35 +0900

    [Basic] 증가수열 만들기(그리디)

--- problem-solving/basic/section4/증가수열 만들기_그리디.py
+++ problem-solving/basic/section4/증가수열 만들기_그리디.py
@@ -1,28 +1,25 @@
 n = int(input())
 nums = list(map(int, input().split()))
-
 lp = 0
-rp = n-1
-last = 0 # 현재 만들어진 수열의 마지막 항
-tmp = []
+rp = n - 1
+last = 0
 res = ""
-
+tmp = list()
 while lp <= rp:
   if nums[lp] > last:
     tmp.append((nums[lp], 'L'))
   if nums[rp] > last:
     tmp.append((nums[rp], 'R'))
+  tmp.sort()
   if len(tmp) == 0:
     break
   else:
-    tmp.sort()
-    res = res + tmp[0][1] # tmp의 첫 번째 tuple의 index가 1인 값
     last = tmp[0][0]
+    res += tmp[0][1]
     if tmp[0][1] == 'L':
       lp += 1
     else:
       rp -= 1
   tmp.clear()
-  
 print(len(res))
 print(res)
