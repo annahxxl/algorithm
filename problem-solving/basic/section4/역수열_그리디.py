093dc8d8a2f09cabf45b3e09510b09fd4473b215
Hanna Lee <8annahxxl@gmail.com>
2021-09-06 13:49:53 +0900

    [Basic] 역수열(그리디)

--- problem-solving/basic/section4/역수열_그리디.py
+++ problem-solving/basic/section4/역수열_그리디.py
@@ -1,15 +1,26 @@
+'''
+# sol1
 n = int(input())
-cnt = list(map(int, input().split()))
-
-seq = [0] * n # 원 수열
-
-for i in range(n):
-  for j in range(n):
-    if cnt[i]==0 and seq[j]==0:
-      seq[j] = i+1
+arr = list(map(int, input().split()))
+result = [0] * (n + 1)
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    if arr[i-1] == 0 and result[j] == 0:
+      result[j] = i
       break
-    elif seq[j] == 0:
-      cnt[i] -= 1
-      
-for x in seq:
-  print(x, end=' ')
+    elif result[j] == 0:
+      arr[i-1] -= 1 # 앞에 만들어야 할 빈 자리 수 check
+for i in range(1, n+1):
+  print(result[i], end=' ')
+'''
+
+# sol2
+n = int(input())
+arr = list(map(int, input().split()))
+arr = arr[::-1]
+res = list()
+for i in arr:
+  res.insert(i, n)
+  n -= 1
+for i in res:
+  print(i, end=' ')
