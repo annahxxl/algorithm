f1d08094bf5dfb241d79902764c63ebd6e37c6b8
Hanna Lee <8annahxxl@gmail.com>
2021-09-07 11:04:54 +0900

    [Basic] 가장 큰 수(스택)

--- problem-solving/basic/section5/가장 큰 수_스택.py
+++ problem-solving/basic/section5/가장 큰 수_스택.py
@@ -1,19 +1,11 @@
-num, m = map(int, input().split())
-
-num = str(num)
-
+n, m = map(int, input().split())
+n = list(map(int, str(n)))
 stack = []
-
-for x in num:
-  x = int(x)
-  while stack and m>0 and stack[-1]<x:
+for x in n:
+  while stack and m > 0 and stack[-1] < x:
     stack.pop()
     m -= 1
   stack.append(x)
-
 if m != 0:
   stack = stack[:-m]
-  
-res = ''.join(map(str, stack))
-  
-print(res)
+print(''.join(map(str, stack)))
