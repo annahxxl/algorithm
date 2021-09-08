dc324dda33239c52e108ce7d87a402d274fb16d2
Hanna Lee <8annahxxl@gmail.com>
2021-09-08 15:46:50 +0900

    [Basic] 쇠막대기(스택)

--- problem-solving/basic/section5/쇠막대기_스택.py
+++ problem-solving/basic/section5/쇠막대기_스택.py
@@ -1,15 +1,13 @@
-s = input()
-stack = []
+ex = input()
 cnt = 0
-
-for i in range(len(s)):
-  if s[i] == '(':
-    stack.append(s[i])
-  else:
+stack = list() # 잘릴 쇠막대기
+for i in range(len(ex)):
+  if ex[i] == ')':
     stack.pop()
-    if s[i-1] == '(':
+    if ex[i-1] == '(': # 레이저
       cnt += len(stack)
-    else:
+    else: # 쇠막대기 끝부분
       cnt += 1
-    
+  else:
+    stack.append(ex[i])
 print(cnt)
