ca7e0c1e96c48be24d335c63074abf69364d55cf
Hanna Lee <8annahxxl@gmail.com>
2021-09-12 11:55:14 +0900

    [Basic] 응급실(큐)

--- problem-solving/basic/section5/응급실_큐.py
+++ problem-solving/basic/section5/응급실_큐.py
@@ -1,13 +1,36 @@
+'''
+# my solution
 from collections import deque
 
 n, m = map(int, input().split())
-p = deque([(pos, val) for pos, val in enumerate(list(map(int, input().split())))]) # 순서와 위험도 tuple로 저장
-cnt = 0 # m번째 환자의 진료 받는 순서
+danger = list(map(int, input().split()))
+wait = deque()
+for i in range(n):
+  wait.append((i, danger[i]))
+cnt = 0
+while True:
+  idx, danger = wait.popleft()
+  for p, d in wait:
+    if danger < d:
+      wait.append((idx, danger))
+      break
+  else:
+    cnt += 1
+    if idx == m:
+      print(cnt)
+      break
+'''
 
-while p:
-  cur = p.popleft() # 현재 대기목록 첫 번째 환자
-  if any(cur[1] < x[1] for x in p): # 위험도 비교
-    p.append(cur)
+# other solution
+from collections import deque
+
+n, m = map(int, input().split())
+q = deque([(pos, val) for pos, val in enumerate(list(map(int, input().split())))]) # enumerate: 인덱스 번호와 컬렉션의 원소를 tuple형태로 반환
+cnt = 0
+while True:
+  cur = q.popleft()
+  if any(cur[1] < x[1] for x in q):
+    q.append(cur)
   else:
     cnt += 1
     if cur[0] == m:
