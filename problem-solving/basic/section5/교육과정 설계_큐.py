5cc0b63e8e5f38b8c944c485a5c1e325d9d29123
Hanna Lee <8annahxxl@gmail.com>
2021-09-13 13:11:40 +0900

    [Basic] 교육과정 설계(큐)

--- problem-solving/basic/section5/교육과정 설계_큐.py
+++ problem-solving/basic/section5/교육과정 설계_큐.py
@@ -1,18 +1,33 @@
+'''
+# my solution
+essential = input()
+n = int(input())
+plan = [input() for _ in range(n)]
+tmp = ''
+for i in range(len(plan)):
+  for j in plan[i]:
+    if j in essential and j not in tmp:
+      tmp += j
+  if tmp == essential:
+    print('#%d YES' %(i+1))
+  else:
+    print('#%d NO' %(i+1))
+  tmp = ''
+'''
+
+# other solution
 from collections import deque
 
 need = input()
 n = int(input())
-
 for i in range(n):
   plan = input()
   dq = deque(need)
-  for x in plan:
-    if x in dq:
-      if x != dq.popleft():
-        print('#%d NO' %(i+1))
+  for s in plan:
+    if s in dq:
+      if s != dq.popleft():
         break
+  if len(dq) == 0:
+    print('#%d YES' %(i+1))
   else:
-    if len(dq) != 0:
-      print('#%d NO' %(i+1))
-    else:
-      print('#%d YES' %(i+1))
+    print('#%d NO' %(i+1))
