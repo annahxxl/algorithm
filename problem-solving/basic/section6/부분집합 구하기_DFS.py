91a9aa5d8e381588b6117048f3524cbf0f8ccda1
Hanna Lee <8annahxxl@gmail.com>
2021-09-19 20:59:56 +0900

    [Basic] 부분집합 구하기(DFS)

--- problem-solving/basic/section6/부분집합 구하기_DFS.py
+++ problem-solving/basic/section6/부분집합 구하기_DFS.py
@@ -1,16 +1,16 @@
 def DFS(v):
-  if v == n + 1:
+  if v == n+1:
     for i in range(1, n+1):
-      if check[i] == 1:
+      if used[i] == 1:
         print(i, end=' ')
     print()
   else:
-    check[v] = 1
+    used[v] = 1
     DFS(v+1)
-    check[v] = 0
+    used[v] = 0
     DFS(v+1)
 
 if __name__ == '__main__':
   n = int(input())
-  check = [0] * (n+1)
+  used = [0] * (n+1)
   DFS(1)
