db0ee4e0934767edcefcf11be03f3ac891c2e5ac
Hanna Lee <8annahxxl@gmail.com>
2021-10-27 21:10:40 +0900

    basic: 계단오르기, 돌다리 건너기

---
+++ problem-solving/basic/section8/계단오르기.py
@@ -0,0 +1,27 @@
+# Bottom-up
+n = int(input())
+dp = [0] * (n + 1)
+dp[1] = 1
+dp[2] = 2
+for i in range(3, n + 1):
+  dp[i] = dp[i - 1] + dp[i - 2]
+print(dp[n])
+
+'''
+# Top-down
+def DFS(step):
+  if dp[step] > 0:
+    return dp[step]
+  if step == 1 or step == 2:
+    return step
+  else:
+    dp[step] = DFS(step - 1) + DFS(step - 2)
+    return dp[step]
+
+if __name__ == '__main__':
+  n = int(input())
+  dp = [0] * (n + 1)
+  dp[1] = 1
+  dp[2] = 2
+  print(DFS(n))
+'''
