f30b1fef07812b4f65f0d7165820bb4a0a6fef5c
Hanna Lee <8annahxxl@gmail.com>
2023-07-17 12:32:51 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    if n > 1:
+        return n * factorial(n - 1)
+    return 1
+
+def solution(balls, share):
+    return factorial(balls) // (factorial(balls - share) * factorial(share))
