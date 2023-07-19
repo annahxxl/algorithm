d424edbfe815822ffd8bb8444d748f634f996a22
Hanna Lee <8annahxxl@gmail.com>
2023-07-19 09:16:15 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,9 +1,3 @@
 def solution(numbers):
-    answer = 0
-    n = len(numbers)
-    
-    for i in range(n):
-        for j in range(i + 1, n):
-            answer = max(answer, numbers[i] * numbers[j])
-    
-    return answer
+    numbers.sort(reverse = True)
+    return numbers[0] * numbers[1]
