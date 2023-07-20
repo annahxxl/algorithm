b667ef33d3ff534753b17ef181c48a4d48a4c1cf
Hanna Lee <8annahxxl@gmail.com>
2023-07-20 10:58:32 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,11 +1,8 @@
 def solution(my_string):
-    check = set()
     answer = ''
 
     for s in my_string:
-        if s not in check:
-            check.add(s)
+        if s not in answer:
             answer += s
-            continue
-        
+            
     return answer
