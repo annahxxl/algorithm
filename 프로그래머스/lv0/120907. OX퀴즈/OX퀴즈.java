07284e0d07f2e022671e9e5b4e333714ce004bb2
Hanna Lee <8annahxxl@gmail.com>
2023-07-27 07:39:11 +0900

    [level 0] Title: OX퀴즈, Time: 0.23 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -5,31 +5,19 @@ import java.util.List;
 class Solution {
     public String[] solution(String[] quiz) {
         List<String> answer = new ArrayList<>();
-        String[][] quizList = new String[quiz.length][2];
         
-        for (int i = 0; i < quiz.length; i++) {
-            quizList[i] = quiz[i].split(" = ");
-        }
-        
-        for (String[] q : quizList) {
-            String[] ex = q[0].split(" ");
-            int a = Integer.parseInt(ex[0]);
-            int b = Integer.parseInt(ex[2]);
-            int c = Integer.parseInt(q[1]);
+        for (String q : quiz) {
+            String[] splitQuiz = q.split(" = ");
+            String[] ex = splitQuiz[0].split(" ");
             
-            if (ex[1].equals("+")) {
-                if (a + b == c) {
-                    answer.add("O");
-                } else {
-                    answer.add("X");
-                }
-            } else {
-                if (a - b == c) {
-                    answer.add("O");
-                } else {
-                    answer.add("X");
-                }
-            }
+            int a = Integer.parseInt(ex[0]);
+            String operator = ex[1];
+            int b = Integer.parseInt(ex[2]);
+            int c = Integer.parseInt(splitQuiz[1]);
+            
+            boolean isCorrect = operator.equals("+") ? a + b == c : a - b == c;
+            
+            answer.add(isCorrect ? "O" : "X");
         }
         
         return answer.toArray(new String[0]);
