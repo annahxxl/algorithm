dfb9a2eb088d1fa0bab5280d4e444e376df550ab
Hanna Lee <8annahxxl@gmail.com>
2023-07-26 09:20:30 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,7 +1,6 @@
 def solution(my_string):
     my_string_list = my_string.split(' ')
     answer = int(my_string_list[0])
-    op = '+'
     
     for i in range(1, len(my_string_list), 2):
         if my_string_list[i] == '+':
