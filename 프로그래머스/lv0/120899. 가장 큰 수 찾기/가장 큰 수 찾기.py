7ff50c338ae819a3bdfdfcd414bc58633a38f79a
Hanna Lee <8annahxxl@gmail.com>
2023-07-26 09:06:03 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -0,0 +1,11 @@
+def solution(array):
+    answer = []
+    idx = 0
+    num = array[idx]
+    
+    for i in range(len(array)):
+        if num < array[i]:
+            idx = i
+            num = array[i]
+            
+    return [num, idx]
