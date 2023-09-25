2c7ecb7ba7a2ad34b008a1d94e401d2ffa1d2a5b
Hanna <hanna@trdst.com>
2023-09-25 13:38:22 +0900

    add: 서로 다른 빈도수 만들기

---
+++ lecture/src/김태원/초급/해싱_and_파싱/서로_다른_빈도수_만들기/Solution.java
@@ -0,0 +1,38 @@
+package 김태원.초급.해싱_and_파싱.서로_다른_빈도수_만들기;
+
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        int answer = 0;
+
+        for (char key : map.keySet()) {
+            while (set.contains(map.get(key))) {
+                answer++;
+                map.put(key, map.get(key) - 1);
+            }
+
+            if (map.get(key) == 0) continue;
+
+            set.add(map.get(key));
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution("aaabbbcc"));
+        System.out.println(T.solution("aaabbc"));
+        System.out.println(T.solution("aebbbbc"));
+        System.out.println(T.solution("aaabbbcccde"));
+        System.out.println(T.solution("aaabbbcccdddeeeeeff"));
+    }
+}
